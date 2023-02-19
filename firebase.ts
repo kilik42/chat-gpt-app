5b8fb1f379aab80525f725582dd2f59cbc216547
kilik42 <maevins@cps.edu>
2023-02-18 20:33:24 -0600

    working on firebase

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,8 @@
 import {getApp, getApps, initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
+import {initializeApp} from "firebase/app";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDPoDe3hq68Qn9V77Jy_exNMH2zL5H7_dM",
     authDomain: "chatgpt-clone-522a6.firebaseapp.com",
@@ -10,3 +12,5 @@ const firebaseConfig = {
     appId: "1:371694531773:web:e2030b0729841258aaf3d4",
     measurementId: "G-0EY5MDL1KD"
   };
+
+  const app = initializeApp(firebaseConfig);
