71e8b7a81507be1f2c62ab9c2f1609af5b97028d
kilik42 <maevins@cps.edu>
2023-02-18 20:31:23 -0600

    working on firebase

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import {getApp, getApps, initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPoDe3hq68Qn9V77Jy_exNMH2zL5H7_dM",
+    authDomain: "chatgpt-clone-522a6.firebaseapp.com",
+    projectId: "chatgpt-clone-522a6",
+    storageBucket: "chatgpt-clone-522a6.appspot.com",
+    messagingSenderId: "371694531773",
+    appId: "1:371694531773:web:e2030b0729841258aaf3d4",
+    measurementId: "G-0EY5MDL1KD"
+  };
