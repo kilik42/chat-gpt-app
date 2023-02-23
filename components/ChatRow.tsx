0806f4625d509ceffb05ab9f87d8450aeac8bcb0
kilik42 <marvinevins@yahoo.com>
2023-02-22 21:29:35 -0600

    working on chatrow

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -5,6 +5,10 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import {useState} from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 type Props = {
   id: string;
 
@@ -13,6 +17,11 @@ function ChatRow({id}: Props) {
   const pathname = usePathname;
   const router = useRouter();
   const {data: session} = useSession();
+  const [active, setActive] = useState(false);
+  const [messages] = useCollection(query(
+    collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'),
+    orderBy('createdAt', 'asc')
+  ))
   
   return (
     <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
